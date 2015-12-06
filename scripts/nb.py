452027b729a8c4059bb62ee69150e813a2a9ad0b
fieteb <fiete.botschen@gmail.com>
2015-12-06 13:24:32 -0500

    fixed crazy repository

--- scripts/nb.py
+++ scripts/nb.py
@@ -1,43 +1,29 @@
-from random import shuffle as shuf;
 import nltk;
 import string;
 import sys;
+from sklearn.svm import LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+
+from tweetLoader import loadNonRacistTweets;
+from tweetLoader import loadRacistTweets;
 
 reload(sys);
 sys.setdefaultencoding("utf8");
 
-excludedWords = ["the", "be", "to", "of", "and", "a", "in", "that", "have", "it", "not", "for", "on", "with", "he", "as", "you", "do"];
-exclude = set(string.punctuation);
-[exclude.add(w) for w in excludedWords];
+excludedWords = set();
+with open("../data/uninterestingWords.txt") as f :
+    for line in f:
+        excludedWords.add(line.strip());
+        
 
-print exclude
+punct = set(string.punctuation);
+
 
 def preprocess(line) :
-    return ''.join(ch for ch in line.lower() if ch not in exclude);
-
-def loadRacistTweets() :
-    fileName = "../data/downloadedRacistTweets.txt";
-
-    tweets = [];
-    with open(fileName) as f:
-        for line in f:
-            tweets.append((preprocess(line).split(), 1));
-
-    shuf(tweets);
-
-    return tweets;
-
-def loadNormalTweets() :
-    fileName = "../data/randomTweets200k.txt";
-
-    tweets = [];
-    with open(fileName) as f:
-        for line in f:
-            tweets.append((preprocess(line).split(), 0));
-
-    shuf(tweets);
-
-    return tweets;
+    # check for excluded words
+    tmp = ' '.join(w for w in line.lower().split(" ") if w not in excludedWords).strip();
+    # check for punctuation
+    return ''.join(ch for ch in tmp if ch not in punct);
 
 def getWords(tupleList):
     res = [];
@@ -46,14 +32,6 @@ def getWords(tupleList):
             res.append(word);
     return res;
 
-#http://stackoverflow.com/questions/14003291/n-grams-with-naive-bayes-classifier
-def bigramReturner (tweetString):
-    tweetString = tweetString.lower()
-    #tweetString = removePunctuation (tweetString)
-    bigramFeatureVector = []
-    for item in nltk.bigrams(tweetString.split()):
-        bigramFeatureVector.append(' '.join(item))
-    return bigramFeatureVector
 
 
 '''
@@ -65,56 +43,64 @@ def bigramReturner (tweetString):
 if __name__ == "__main__" :
     print("NB start");
     racistTweets = loadRacistTweets();
-    normalTweets = loadNormalTweets();
-
+    normalTweets = loadNonRacistTweets(numTweets=len(racistTweets));
+         
     print("Number of racist tweets: {}.".format(len(racistTweets)));
     print("Number of normal tweets: {}.".format(len(normalTweets)));
-
-    trainR = racistTweets[0:4000];
-    testR = racistTweets[4000:-1];
-
-    numTrain = 4000;
-    numTest = 2308;
+    
+    numTrain = 3000;
+    numTest = 1500; 
+     
+    trainR = racistTweets[0:numTrain];
+    testR = racistTweets[numTrain:numTrain + numTest];
+     
     trainN = normalTweets[0:numTrain];
     testN = normalTweets[numTrain:numTrain + numTest];
-
-
+    
+    
     trainTweets = trainR + trainN;
     testTweets = testR + testN;
-
+    
     trainWords = getWords(trainTweets);
     wordFreqs = nltk.FreqDist(w for w in trainWords);
-
+    
     # plot of most common words
-    wordFreqs.plot(50, cumulative=False);
-
+    # wordFreqs.plot(50, cumulative=False);
+    
     wordFeatures = list(wordFreqs)[:1000];
-
-    def getFeatures(document) :
+    
+    def getFeatures(document) : 
         documentWords = set(document);
         features = {};
-        for pair in bigramReturner(' '.join(documentWords)):
-            features['contains_pair({})'.format(pair)] = 1
         for word in wordFeatures:
             features['contains({})'.format(word)] = (word in documentWords);
         return features
-
-
+    
+         
     trainFeats = [(getFeatures(d), c) for (d,c) in trainTweets];
     testFeats = [(getFeatures(d), c) for (d,c) in testTweets];
-
-    print trainFeats[-2]
-    print testFeats[-2]
-
+    
+    # Bayes
     nbClass = nltk.NaiveBayesClassifier.train(trainFeats);
-
-
+    
+    print("----------------------");
+    print("Naive Bayes Classifier");
     print(nltk.classify.accuracy(nbClass, testFeats));
-
+     
     nbClass.show_most_informative_features(10);
-
-    print("NB end");
-
-    #64.14% on bigram features - BoW
-    #73% on unigram features - BoW
-    #81.45%
+    
+    # SVM
+    svmClass = nltk.classify.SklearnClassifier(LinearSVC());
+    svmClass.train(trainFeats);
+    
+    print("----------------------");
+    print("SVM Classifier");
+    print(nltk.classify.accuracy(svmClass, testFeats));
+    
+    # RF
+    rfClass = nltk.classify.SklearnClassifier(RandomForestClassifier(n_estimators = 100));
+    rfClass.train(trainFeats);
+    
+    print("----------------------");
+    print("RF Classifier");
+    print(nltk.classify.accuracy(rfClass, testFeats));
