f812f85f729a0e8c5fbfe6046a1aa3eaf55f9365
giffy1 <giffy1991@gmail.com>
2015-12-17 17:35:53 -0500

    Changed name of preprocess method to match actual method

--- scripts/nb.py
+++ scripts/nb.py
@@ -7,7 +7,7 @@ from sklearn.ensemble import RandomForestClassifier
 from tweetLoader import loadNonRacistTweets;
 from tweetLoader import loadRacistTweets;
 
-from preprocessing import nbPreprocess;
+from preprocessing import preprocess;
 
 reload(sys);
 sys.setdefaultencoding("utf8");
@@ -16,7 +16,7 @@ excludedWords = set();
 with open("../data/uninterestingWords.txt") as f :
     for line in f:
         excludedWords.add(line.strip());
-        
+
 
 punct = set(string.punctuation);
 
@@ -37,63 +37,63 @@ def getWords(tupleList):
 '''
 if __name__ == "__main__" :
     print("NB start");
-    racistTweets = [(nbPreprocess(d), c) for (d, c) in loadRacistTweets(excludeJokes=True)];
-    normalTweets = [(nbPreprocess(d), c) for (d, c) in loadNonRacistTweets(numTweets=len(racistTweets))];   
+    racistTweets = [(preprocess(d), c) for (d, c) in loadRacistTweets(excludeJokes=True)];
+    normalTweets = [(preprocess(d), c) for (d, c) in loadNonRacistTweets(numTweets=len(racistTweets))];
 
     print("Number of racist tweets: {}.".format(len(racistTweets)));
     print("Number of normal tweets: {}.".format(len(normalTweets)));
-    
+
     numTrain = 1500;
-    numTest = 500; 
-     
+    numTest = 500;
+
     trainR = racistTweets[0:numTrain];
     testR = racistTweets[numTrain:numTrain + numTest];
-     
+
     trainN = normalTweets[0:numTrain];
     testN = normalTweets[numTrain:numTrain + numTest];
-    
-    
+
+
     trainTweets = trainR + trainN;
     testTweets = testR + testN;
-    
+
     trainWords = getWords(trainTweets);
     wordFreqs = nltk.FreqDist(w for w in trainWords);
-    
+
     # plot of most common words
     # wordFreqs.plot(50, cumulative=False);
-    
+
     wordFeatures = list(wordFreqs)[:1000];
-    
-    def getFeatures(document) : 
+
+    def getFeatures(document) :
         documentWords = set(document);
         features = {};
         for word in wordFeatures:
             features['contains({})'.format(word)] = (word in documentWords);
         return features
-    
-         
+
+
     trainFeats = [(getFeatures(d), c) for (d,c) in trainTweets];
     testFeats = [(getFeatures(d), c) for (d,c) in testTweets];
-    
+
     # Bayes
     nbClass = nltk.NaiveBayesClassifier.train(trainFeats);
-    
+
     print("----------------------");
     print("Naive Bayes Classifier");
     print(nltk.classify.accuracy(nbClass, testFeats));
-         
+
     # SVM
     svmClass = nltk.classify.SklearnClassifier(LinearSVC());
     svmClass.train(trainFeats);
-    
+
     print("----------------------");
     print("SVM Classifier");
     print(nltk.classify.accuracy(svmClass, testFeats));
-    
+
     # RF
     rfClass = nltk.classify.SklearnClassifier(RandomForestClassifier(n_estimators = 100));
     rfClass.train(trainFeats);
-    
+
     print("----------------------");
     print("RF Classifier");
     print(nltk.classify.accuracy(rfClass, testFeats));
