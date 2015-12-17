5ccbd7c43b9a5c9ae9e851f1f86f8ad43ff76325
giffy1 <giffy1991@gmail.com>
2015-12-17 17:36:28 -0500

    Minor changes to this

--- scripts/tweetClassifierSVM.py
+++ scripts/tweetClassifierSVM.py
@@ -41,7 +41,7 @@ def precision_recall_fscore(confusion_matrix, class_index):
 #
 # ----------------------------------------------------------------------
 
-def evaluate_classifier (numTrainR, numTrainN, numTestR, numTestN):
+def evaluate_classifier (numTrainR, numTrainN, numTestR, numTestN, verbose):
     '''
         I used code from
         http://www.nltk.org/book/ch06.html
@@ -52,32 +52,36 @@ def evaluate_classifier (numTrainR, numTrainN, numTestR, numTestN):
     #load raw tweets:
     rawRacistTweets = loadRacistTweets(numTweets = numTrainR + numTestR, excludeJokes=True)
     rawNormalTweets = loadNonRacistTweets(numTweets = numTrainN + numTestN)
-    rawTweets = rawRacistTweets + rawNormalTweets
+    #rawTweets = rawRacistTweets + rawNormalTweets
 
-    #pre-process tweets (i.e. remove certain words):
-    racistTweets = [(preprocess(d), c) for (d, c) in rawRacistTweets];
-    normalTweets = [(preprocess(d), c) for (d, c) in rawNormalTweets];
-
-    print("Number of racist tweets: {}.".format(len(racistTweets)));
-    print("Number of normal tweets: {}.".format(len(normalTweets)));
+    print("Number of racist tweets: {}.".format(len(rawRacistTweets)));
+    print("Number of normal tweets: {}.".format(len(rawNormalTweets)));
 
     #split into train/test sets
-    trainR = racistTweets[0:numTrainR];
-    testR = racistTweets[numTrainR:numTrainR + numTestR];
+    trainR = rawRacistTweets[0:numTrainR];
+    print(len(trainR))
+    testR = rawRacistTweets[numTrainR:numTrainR + numTestR];
+    print(len(testR))
 
-    trainN = normalTweets[0:numTrainN];
-    testN = normalTweets[numTrainN:numTrainN + numTestN];
+    trainN = rawNormalTweets[0:numTrainN];
+    print(len(trainN))
+    testN = rawNormalTweets[numTrainN:numTrainN + numTestN];
+    print(len(testN))
 
     #combine racist/non-racist tweets into single train/test datasets
     trainTweets = trainR + trainN;
     testTweets = testR + testN;
 
-    featureExtractor = FeatureExtractor(FeatureExtractor.TF_IDF)
+    #pre-process tweets (i.e. remove certain words):
+    preprocessedTrainTweets = [(preprocess(d), c) for (d, c) in trainTweets];
+    preprocessedTestTweets = [(preprocess(d), c) for (d, c) in testTweets];
+
+    featureExtractor = FeatureExtractor([FeatureExtractor.UNIGRAM, FeatureExtractor.BIGRAM, FeatureExtractor.TF_IDF])
     featureExtractor.train_TF_IDF(trainTweets)
 
     #compute training & testing features
-    trainFeats = [(featureExtractor.get_feature_vector(d), c) for (d,c) in trainTweets];
-    testFeats = [(featureExtractor.get_feature_vector(d), c) for (d,c) in testTweets];
+    trainFeats = [(featureExtractor.get_feature_vector(d), c) for (d,c) in preprocessedTrainTweets];
+    testFeats = [(featureExtractor.get_feature_vector(d), c) for (d,c) in preprocessedTestTweets];
 
     svmClass = nltk.classify.SklearnClassifier(LinearSVC());
     svmClass.train(trainFeats);
@@ -97,14 +101,18 @@ def evaluate_classifier (numTrainR, numTrainN, numTestR, numTestN):
     print("precision: %.3f" %precision)
     print("recall: %.3f" %recall)
     print("f1 score: %.3f" %fscore)
+    print("%.1f\%% & %.1f\%% & %.1f\%%" %(100*precision,100*recall,100*fscore))
 
     print("confusion matrix:")
     print(conf)
 
-    FP_index = np.where(np.subtract(Y_pred, Y_test)==1)[0][0]
-    FN_index = np.where(np.subtract(Y_pred, Y_test)==-1)[0][0]
-    print("False positive: {}".format(' '.join(rawTweets[FP_index][0])))
-    print("False negative: {}".format(' '.join(rawTweets[FN_index][0])))
+    if verbose:
+        FP_indeces = np.where(np.subtract(Y_pred, Y_test)==1)[0]
+        FN_indeces = np.where(np.subtract(Y_pred, Y_test)==-1)[0]
+        for FP_index in FP_indeces:
+            print("False positive: {}".format(' '.join(testTweets[FP_index][0])))
+        for FN_index in FN_indeces:
+            print("False negative: {}".format(' '.join(testTweets[FN_index][0])))
 
 #%% --------------------------------------------------------------------
 #
@@ -112,12 +120,12 @@ def evaluate_classifier (numTrainR, numTrainN, numTestR, numTestN):
 #
 # ----------------------------------------------------------------------
 
-def main(numTrainR, numTrainN, numTestR, numTestN):
+def main(numTrainR, numTrainN, numTestR, numTestN, verbose):
     '''
     Main method: calls evaluate_classifier() and shows time elapsed
     '''
     t0 = time()
-    evaluate_classifier(numTrainR, numTrainN, numTestR, numTestN)
+    evaluate_classifier(numTrainR, numTrainN, numTestR, numTestN, verbose)
 
     tf = time()
     print "Elapsed time: {} seconds".format(tf - t0)
@@ -131,13 +139,15 @@ def main(numTrainR, numTrainN, numTestR, numTestN):
 if __name__ == "__main__" :
     parser = ArgumentParser()
     parser.add_argument("--n-racist-train", type=int, dest="numTrainR",
-                default=1500, help="Number of racist tweets in the training set")
+                default=1000, help="Number of racist tweets in the training set")
     parser.add_argument("--n-normal-train", type=int, dest="numTrainN",
-                default=8000, help="Number of non-racist tweets in the training set")
+                default=1000, help="Number of non-racist tweets in the training set")
     parser.add_argument("--n-racist-test", type=int, dest="numTestR",
-                default=1500, help="Number of racist tweets in the test set")
+                default=100, help="Number of racist tweets in the test set")
     parser.add_argument("--n-normal-test", type=int, dest="numTestN",
-                default=8000, help="Number of non-racist tweets in the test set")
+                default=1900, help="Number of non-racist tweets in the test set")
+    parser.add_argument("--verbose", type=bool, dest="verbose",
+                default=False, help="If verbose=True, print wrong classifications")
 
     args = parser.parse_args()
 
